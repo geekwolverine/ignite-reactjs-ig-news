8040f9db9e6b1449d3a630f94421ad3ebb18fd2b
Luke Morales <lukemorales@live.com>
2021-03-23 00:05:04 -0300

    :sparkles: feat: create Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
 import { MetaTags } from '../components';
+import * as S from '../styles/home';
 
 export default function Home() {
   return (
     <>
       <MetaTags title="Home" />
-      <h1>Hello World</h1>
+
+      <S.Container>
+        <S.Section>
+          <span>👏 Hey, welcome</span>
+
+          <h1>
+            News about <br /> the <strong>React</strong> world{' '}
+          </h1>
+
+          <p>
+            Get access to all the articles <strong>for U$9.90/month</strong>{' '}
+          </p>
+        </S.Section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </S.Container>
     </>
   );
 }
---
+++ src/styles/home.ts
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.main`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1rem;
+    margin: 0 auto;
+    padding: 2rem;
+    width: 100%;
+    max-width: 63.75rem;
+    height: calc(100vh - 5rem);
+    color: ${theme.colors.text};
+  `}
+`;
+
+export const Section = styled.section`
+  ${({ theme }) => css`
+    font-size: 1.5rem;
+    font-weight: 700;
+
+    > h1 {
+      margin: 2.5rem 0 1.25rem;
+      font-size: 4.5rem;
+      font-weight: 900;
+      line-height: 4.5rem;
+      color: ${theme.colors.titles};
+
+      > strong {
+        color ${theme.colors.blue};
+      }
+    }
+
+    > p {
+      font-weight: normal;
+
+      > strong {
+        display: block;
+        color ${theme.colors.blue};
+      }
+    }
+  `}
+`;
