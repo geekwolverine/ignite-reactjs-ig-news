8e8efb4fcd3f56bda4d3a4433e7fb43e1025961d
Luke Morales <lukemorales@live.com>
2021-03-25 22:17:30 -0300

    :sparkles: feat: login user with github auth

--- src/components/AuthButton/index.tsx
+++ src/components/AuthButton/index.tsx
@@ -1,23 +1,28 @@
-import { useState } from 'react';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import { useTheme } from 'styled-components';
 import { GoMarkGithub, GoX } from 'react-icons/go';
 
 import * as S from './styles';
 
+const handleSignIn = () => signIn('github');
+const handleSignOut = () => signOut();
+
 export const AuthButton = () => {
   const theme = useTheme();
 
-  const [isLoggedIn] = useState(false);
+  const [session] = useSession();
 
-  const githubIconColor = theme.colors[isLoggedIn ? 'green' : 'yellow'];
+  const handleAuth = session ? handleSignOut : handleSignIn;
+
+  const githubIconColor = theme.colors[session ? 'green' : 'yellow'];
 
   return (
-    <S.Container type="button">
+    <S.Container type="button" onClick={handleAuth}>
       <GoMarkGithub color={githubIconColor} size="1.25rem" />
-      <span>Sign in with GitHub</span>
+      <span>{session?.user?.name ?? 'Sign in with GitHub'}</span>
 
-      {isLoggedIn && <GoX color={theme.colors.detailLight} size="1.25rem" />}
+      {session && <GoX color={theme.colors.detailLight} size="1.25rem" />}
     </S.Container>
   );
 };
