ed05211fa07a8ab2fe7859d5df1efe81f57eb622
Luke Morales <lukemorales@live.com>
2021-03-31 19:41:52 -0300

    :sparkles: feat: store logged user in faunadb

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import { Provider as NextAuthProvider } from 'next-auth/client';
+import { StrictMode } from 'react';
 
 import { ThemeProvider } from 'styled-components';
 
@@ -9,13 +10,15 @@ import { Header } from '../components';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextAuthProvider session={pageProps.session}>
-      <ThemeProvider theme={theme}>
-        <Header />
-        <Component {...pageProps} />
-        <GlobalStyles />
-      </ThemeProvider>
-    </NextAuthProvider>
+    <StrictMode>
+      <NextAuthProvider session={pageProps.session}>
+        <ThemeProvider theme={theme}>
+          <Header />
+          <Component {...pageProps} />
+          <GlobalStyles />
+        </ThemeProvider>
+      </NextAuthProvider>
+    </StrictMode>
   );
 }
 
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,10 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services';
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -10,4 +14,25 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  callbacks: {
+    async signIn(user) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email)),
+              ),
+            ),
+            q.Create(q.Collection('users'), { data: { email: user.email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email))),
+          ),
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
